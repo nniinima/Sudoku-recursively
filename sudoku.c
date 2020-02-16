bfcd6dac7773d1985db956957941cc1fa9893a8e
Niko Niinimaki <niko.niinimaki@gmail.com>
2020-02-16 12:07:45 +0200

    algorithmically awesome

--- sudoku.c
+++ sudoku.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-//sudoku problem
-int matrix[9][9] = {
+//global sudoku problem
+int g_matrix[9][9] = {
     {6,5,0,8,7,3,0,9,0},
     {0,0,3,2,5,0,0,0,8},
     {9,8,0,1,0,4,3,5,7},
@@ -13,34 +13,41 @@ int matrix[9][9] = {
     {0,9,0,6,2,5,0,8,1}
 };
 
-//function to print sudoku
+//Sudoku printer suite
 void ft_sudoku_print()
 {
-    int i,j;
-    for(i=0;i<9;i++)
+    int i;
+    int j;
+
+    i = 0;
+    while (i < 9)
     {
-        for(j=0;j<9;j++)
+        j = 0;
+        while (j < 9)
         {
-            printf("%d ",matrix[i][j]);
+            printf("%d ",g_matrix[i][j]);
+            j++;
         }
         printf("\n");
+        i++;
     }
 }
 
-//function to check if all cells are assigned or not
-//if there is any unassigned cell
-//then this function will change the values of
-//row and col accordingly
+//here we check if the cells are assigned or not//
 int ft_cell_unassigned(int *row, int *col)
 {
     int num_unassign = 0;
-    int i,j;
-    for(i=0;i<9;i++)
+    int i;
+    int j;
+
+    i = 0;
+    while (i < 9)
     {
-        for(j=0;j<9;j++)
+        j = 0;
+        while (j < 9)
         {
             //cell is unassigned
-            if(matrix[i][j] == 0)
+            if(g_matrix[i][j] == 0)
             {
                 //changing the values of row and col
                 *row = i;
@@ -49,40 +56,53 @@ int ft_cell_unassigned(int *row, int *col)
                 num_unassign = 1;
                 return num_unassign;
             }
+            j++;
         }
+        i++;
     }
     return num_unassign;
 }
 
-//function to check if we can put a
-//value in a paticular cell or not
+//number checker, can we assign a particular number or not?
 int ft_check_number(int n, int r, int c)
 {
-    int i,j;
-    //checking in row
-    for(i=0;i<9;i++)
+    int i;
+    int j;
+    int row_start;
+    int col_start;
+
+    i = 0;
+    while (i < 9)
     {
         //there is a cell with same value
-        if(matrix[r][i] == n)
+        if(g_matrix[r][i] == n)
             return 0;
+        i++;
     }
     //checking column
-    for(i=0;i<9;i++)
+    i = 0;
+    while (i < 9)
     {
         //there is a cell with the value equal to i
-        if(matrix[i][c] == n)
+        if(g_matrix[i][c] == n)
             return 0;
+        i++;
     }
-    //checking sub matrix
-    int row_start = (r/3)*3;
-    int col_start = (c/3)*3;
-    for(i=row_start;i<row_start+3;i++)
+    //checking sub g_matrix
+    row_start = (r/3)*3;
+    col_start = (c/3)*3;
+
+    i = row_start;
+    while (i < row_start + 3)
     {
-        for(j=col_start;j<col_start+3;j++)
+        j = col_start;
+        while(j < col_start + 3)
         {
-            if(matrix[i][j]==n)
+            if(g_matrix[i][j]==n)
                 return 0;
+            j++;
         }
+        i++;
     }
     return 1;
 }
@@ -91,28 +111,31 @@ int ft_check_number(int n, int r, int c)
 //using backtracking
 int ft_backtracking_algorithm()
 {
+    int n;
+    int i;
     int row;
     int col;
     //if all cells are assigned then the sudoku is already solved
     //pass by reference because ft_cell_unassigned will change the values of row and col
     if(ft_cell_unassigned(&row, &col) == 0)
         return 1;
-    int n,i;
     //number between 1 to 9
-    for(i=1;i<=9;i++)
+    i = 1;
+    while (i <= 9)
     {
         //if we can assign i to the cell or not
-        //the cell is matrix[row][col]
+        //the cell is g_matrix[row][col]
         if(ft_check_number(i, row, col))
         {
-            matrix[row][col] = i;
+            g_matrix[row][col] = i;
             //backtracking
             if(ft_backtracking_algorithm())
                 return 1;
             //if we can't proceed with this solution
             //reassign the cell
-            matrix[row][col]=0;
+            g_matrix[row][col]=0;
         }
+        i++;
     }
     return 0;
 }
